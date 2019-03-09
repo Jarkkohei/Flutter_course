44ee5a80cbc77cff6f2b3ea10a38dd0c88e6fdad
Jarkko <jarkkohei@luukku.com>
2019-03-09 22:25:03 +0200

    Create style theme and use it in widgets

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurpleAccent,
+        primaryColorLight: Colors.white
+      ),
       home: Scaffold(
           appBar: AppBar(
             title: Text('EasyList'),
--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -28,6 +28,8 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).primaryColorLight,
               onPressed: () {
                 setState(() {
                   _products.add('Advanced Food Tester');
