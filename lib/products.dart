355d29b32f2fc7a7faa523a89224af10cf954320
Jarkko <jarkkohei@luukku.com>
2019-03-10 12:22:38 +0200

    Fix semantics on productCard to plural

--- lib/products.dart
+++ lib/products.dart
@@ -17,19 +17,19 @@ class Products extends StatelessWidget {
   }
 
   Widget _buildProductList() {
-    Widget productCard;
+    Widget productCards;
 
     if(products.length > 0) {
-      productCard = ListView.builder(
+      productCards = ListView.builder(
         itemBuilder: _buildProductItem,
         itemCount: products.length,
       );
     } else {
-      productCard = Center(
+      productCards = Center(
         child: Text('No products found, please add some.')
       );
     }
-    return productCard;
+    return productCards;
   }
 
   @override
