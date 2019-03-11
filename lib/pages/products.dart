b695e79df8c066bf3bbcc60394570d0f236efdd8
Jarkko <jarkkohei@luukku.com>
2019-03-11 15:19:19 +0200

    Add a product_admin-page and a drawer with link to it

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 import '../product_manager.dart';
+import './products_admin.dart';
 
 class ProductsPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('EasyList'),
-      ),
-      body: ProductManager()
-    );
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('Choose'),
+              ),
+              ListTile(
+                title: Text('Manage products'),
+                onTap: () => {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (BuildContext context) => ProductsAdmin(),
+                        ),
+                      ),
+                    },
+              ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager());
   }
 }
