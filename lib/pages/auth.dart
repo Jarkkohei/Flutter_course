9e928b4ac48ed645f3e17ebd0e6dda82a155836c
Jarkko <jarkkohei@luukku.com>
2019-03-12 17:45:34 +0200

    Add Accept terms-switch to Login-page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -13,6 +13,7 @@ class AuthPage extends StatefulWidget {
 class _AuthPageState extends State<AuthPage> {
   String _emailValue;
   String _passwordValue;
+  bool _acceptTerms = false;
 
   @override
   Widget build(BuildContext context) {
@@ -42,6 +43,14 @@ class _AuthPageState extends State<AuthPage> {
                 });
               },
             ),
+            SwitchListTile(
+              value: _acceptTerms, 
+              onChanged: (bool value) {
+                setState(() {
+                  _acceptTerms = value;
+                });
+              }, 
+              title: Text('Accept terms'),),
             SizedBox(height: 10.0),
             RaisedButton(
               color: Theme.of(context).primaryColor,
