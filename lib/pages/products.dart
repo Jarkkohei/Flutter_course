ab216cd9382a8f12ca73574cad09788b6f94c89c
Jarkko <jarkkohei@luukku.com>
2019-03-15 14:39:39 +0200

    Optimize products-page

--- lib/pages/products.dart
+++ lib/pages/products.dart
@@ -6,36 +6,39 @@ class ProductsPage extends StatelessWidget {
 
   ProductsPage(this.products);
 
+  Widget _buildSideDrawer(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            automaticallyImplyLeading: false,
+            title: Text('Choose'),
+          ),
+          ListTile(
+            leading: Icon(Icons.edit),
+            title: Text('Manage products'),
+            onTap: () => {
+                  Navigator.pushReplacementNamed(context, '/admin'),
+                },
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(
-              automaticallyImplyLeading: false,
-              title: Text('Choose'),
-            ),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text('Manage products'),
-              onTap: () => {
-                    Navigator.pushReplacementNamed(context, '/admin'),
-                  },
+        drawer: _buildSideDrawer(context),
+        appBar: AppBar(
+          title: Text('EasyList'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
             ),
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: Text('EasyList'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
-          ),
-
-        ],
-      ),
-      body: Products(products));
+        body: Products(products));
   }
 }
