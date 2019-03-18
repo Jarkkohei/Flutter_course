e7705dd685c7e98f13c3d83a0236a9fc6b7aa169
Jarkko <jarkkohei@luukku.com>
2019-03-18 12:23:57 +0200

    Implement hidin the keyboard on CreateProduct-page by adding a onTap GestureDetector

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -25,7 +25,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         /*if(value.trim().length <= 0) {
           return 'Title is required';
         }*/
-        if(value.isEmpty || value.length < 3) {
+        if (value.isEmpty || value.length < 3) {
           return 'Title is required and should be at least 3 characters long.';
         }
       },
@@ -42,7 +42,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       decoration: InputDecoration(labelText: 'Product description'),
       maxLines: 5,
       validator: (String value) {
-        if(value.isEmpty || value.length < 5) {
+        if (value.isEmpty || value.length < 5) {
           return 'Description is required and should be at least 5 characters long.';
         }
       },
@@ -59,7 +59,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
       decoration: InputDecoration(labelText: 'Product price'),
       keyboardType: TextInputType.number,
       validator: (String value) {
-        if(value.isEmpty || !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
+        if (value.isEmpty ||
+            !RegExp(r'^(?:[1-9]\d*|0)?(?:\.\d+)?$').hasMatch(value)) {
           return 'Price is required and should be a number.';
         }
       },
@@ -72,7 +73,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   }
 
   void _submitForm() {
-    if(!_formKey.currentState.validate()) {
+    if (!_formKey.currentState.validate()) {
       return;
     }
     _formKey.currentState.save();
@@ -92,22 +93,27 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
     final double targetWidth = deviceWidth > 550.0 ? 500.0 : deviceWidth * 0.95;
     final double targetPadding = deviceWidth - targetWidth;
 
-    return Container(
-      margin: EdgeInsets.all(10.0),
-      child: Form(
-        key: _formKey,
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
-          children: <Widget>[
-            _buildTitleTextField(),
-            _buildDescriptionTextField(),
-            _buildPriceTextField(),
-            SizedBox(height: 10.0),
-            RaisedButton(
-                textColor: Theme.of(context).primaryColorLight,
-                child: Text('Save'),
-                onPressed: _submitForm),
-          ],
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: targetPadding / 2),
+            children: <Widget>[
+              _buildTitleTextField(),
+              _buildDescriptionTextField(),
+              _buildPriceTextField(),
+              SizedBox(height: 10.0),
+              RaisedButton(
+                  textColor: Theme.of(context).primaryColorLight,
+                  child: Text('Save'),
+                  onPressed: _submitForm),
+            ],
+          ),
         ),
       ),
     );
