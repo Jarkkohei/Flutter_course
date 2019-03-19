0c30104b75817059be7943c2d1def437abacd3e8
Jarkko <jarkkohei@luukku.com>
2019-03-19 12:02:23 +0200

    Add selectedProductIndex and a getter for it in products scoped-model

--- lib/scoped-models/products.dart
+++ lib/scoped-models/products.dart
@@ -3,11 +3,16 @@ import '../models/product.dart';
 
 class ProductsModel extends Model {
   List<Product> _products = [];
+  int _selectedProductIndex;
 
   // Getter for the products list.
   List<Product> get products {
     // Create new list not just a pointer.
     return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selectedProductIndex;
   }
 
   void addProduct(Product product) {
